c4c9c6886d3d1e42ce230ff4802867572bb0dd90
Yatinparmar <99137593+Yatinparmar@users.noreply.github.com>
2024-09-01 20:52:57 +0000

    datatypes learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+const accountID = 123466
+let accountEmail = "yatin@Abc.com"
+var accountPassword = "12345"
+accountCity ="lunawada"
+let accountstate;
+
+// accountID = 2 // not allowed
+
+accountEmail = "john@xyz.com"
+accountPassword = "5896"
+accountCity = "Ahemdabad"
+
+console.log(accountID);
+
+/* 
+Prefer not to use var
+Beacuse of issue in block scope and functional scope
+*/
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountstate])
+
+
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 2) we are using the node.js not browser
+
+/*console.log("3
+    
+    +3")  code relability high */
+
+    
+console.log("yatin");
+
+let name = "yatin"
+let age = 22
+let isloggedIn = false
+let state
+
+//primitive data types 
+//number => to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//non primitive
+
+//object
+
+
+console.log(typeof "yatin");
+console.log(typeof "undefined"); 
+console.log(typeof null); // object 
+
+
